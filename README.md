eea50dfd345cddce4b38b37fe0b4e1b5509e1a67
Passkou <psk116@outlook.com>
2020-09-10 12:44:11 +0800

    First-commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# FontAwesomeProDownloader
+
+著名的字体图标 [Font Awesome](https://fontawesome.com/) Pro版本的下载脚本。
+
+Pro本身是付费的，请不要用于商业用途，下载仅供个人研究和学习使用。
+
+（不用英语是因为怕被举报2333）
+
+因个人使用不当产生的侵权需要自行承担后果。
+
+# 使用第三方库
+
+aiohttp
+
+# 使用方法
+
+运行脚本，会自动下载css文件以及依赖的字体文件。
+
+会自动生成以下结构，请勿改变结构。
+
+```
+css
+-- pro.min.css
+
+webfonts
+-- 各种字体
+```
+
+然后在HTML标签中引入 `css/pro.min.css` 就可以正常使用了，具体用法请去看官方文档。
---
+++ font_awesome_pro_downloader.py
@@ -0,0 +1,77 @@
+"""
+自动下载Font-Awesome-Pro的字体文件和CSS文件
+注意，Pro本身需要付费，喜欢的话还请去支持正版
+https://fontawesome.com/
+"""
+import aiohttp
+import re
+import os
+import asyncio
+
+# 下载路径
+PATH = "https://kit-pro.fontawesome.com/releases/v5.14.0"
+# 下载异步任务数量
+TASK_NUMBER = 100
+
+
+async def main():
+    if not os.path.exists("/css/pro.min.css"):
+        if not os.path.exists("./css"):
+            os.mkdir("./css")
+        # 下载CSS文件
+        print("CSS文件不存在，正在下载")
+        async with aiohttp.ClientSession() as session:
+            await downloader(["/css/pro.min.css"], session)
+
+    # 分析CSS文件
+    with open("./css/pro.min.css") as f:
+        css = f.read()
+
+    if not os.path.exists("./webfonts"):
+        os.mkdir("./webfonts")
+
+    pattern = re.compile("url\(\.\.(/webfonts/.*?)\)")
+    results = re.findall(pattern, css)
+
+    def filter_(value):
+        if value.endswith("eot") or value.endswith("woff") or value.endswith("woff2"):
+            return True
+        return False
+    # 删除多余URL
+    results = list(filter(filter_, results))
+    # 去重
+    results = list(set(results))
+    # 分配任务
+    length = len(results)
+    print("共需要下载：", length, "个文件")
+    task_missions = []
+    for i in range(TASK_NUMBER):
+        task_missions.append([])
+    needle = 0
+    while len(results) > 0:
+        task_mission = results.pop()
+        task_missions[needle].append(task_mission)
+        needle += 1
+        if needle >= TASK_NUMBER:
+            needle = 0
+    async with aiohttp.ClientSession() as session:
+        furthers = []
+        for mission in task_missions:
+            further = downloader(mission, session)
+            furthers.append(further)
+        await asyncio.gather(*furthers)
+
+
+async def downloader(urls, session):
+    for url in urls:
+        async with session.get(PATH + url) as resp:
+            resp.raise_for_status()
+            save_path = "." + url
+            with open(save_path, "wb") as f:
+
+                f.write(await resp.read())
+                print("保存成功", url)
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
